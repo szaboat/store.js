072b04b8be3b856e050a5a6461deb3ee0ef2a053
szaboat <attila.szabo@gmail.com>
2012-04-05 00:34:42 +0200

    Make jshint happy

--- store.js
+++ store.js
@@ -19,91 +19,91 @@
  * THE SOFTWARE.
  */
 
-;(function(){
+(function(){
 	var store = {},
 		win = window,
 		doc = win.document,
 		localStorageName = 'localStorage',
 		globalStorageName = 'globalStorage',
 		namespace = '__storejs__',
-		storage
+		storage;
 
-	store.disabled = false
-	store.set = function(key, value) {}
-	store.get = function(key) {}
-	store.remove = function(key) {}
-	store.clear = function() {}
+	store.disabled = false;
+	store.set = function(key, value) {};
+	store.get = function(key) {};
+	store.remove = function(key) {};
+	store.clear = function() {};
 	store.transact = function(key, defaultVal, transactionFn) {
-		var val = store.get(key)
-		if (transactionFn == null) {
-			transactionFn = defaultVal
-			defaultVal = null
+		var val = store.get(key);
+		if (transactionFn === null) {
+			transactionFn = defaultVal;
+			defaultVal = null;
 		}
-		if (typeof val == 'undefined') { val = defaultVal || {} }
-		transactionFn(val)
-		store.set(key, val)
-	}
-	store.getAll = function() {}
+		if (typeof val == 'undefined') { val = defaultVal || {}; }
+		transactionFn(val);
+		store.set(key, val);
+	};
+	store.getAll = function() {};
 
 	store.serialize = function(value) {
-		return JSON.stringify(value)
-	}
+		return JSON.stringify(value);
+	};
 	store.deserialize = function(value) {
-		if (typeof value != 'string') { return undefined }
-		return JSON.parse(value)
-	}
+		if (typeof value != 'string') { return undefined; }
+		return JSON.parse(value);
+	};
 
 	// Functions to encapsulate questionable FireFox 3.6.13 behavior
 	// when about.config::dom.storage.enabled === false
 	// See https://github.com/marcuswestin/store.js/issues#issue/13
 	function isLocalStorageNameSupported() {
-		try { return (localStorageName in win && win[localStorageName]) }
-		catch(err) { return false }
+		try { return (localStorageName in win && win[localStorageName]); }
+		catch(err) { return false; }
 	}
 
 	function isGlobalStorageNameSupported() {
-		try { return (globalStorageName in win && win[globalStorageName] && win[globalStorageName][win.location.hostname]) }
-		catch(err) { return false }
+		try { return (globalStorageName in win && win[globalStorageName] && win[globalStorageName][win.location.hostname]); }
+		catch(err) { return false; }
 	}
 
 	if (isLocalStorageNameSupported()) {
-		storage = win[localStorageName]
+		storage = win[localStorageName];
 		store.set = function(key, val) {
-			if (val === undefined) { return store.remove(key) }
-			storage.setItem(key, store.serialize(val))
-		}
-		store.get = function(key) { return store.deserialize(storage.getItem(key)) }
-		store.remove = function(key) { storage.removeItem(key) }
-		store.clear = function() { storage.clear() }
+			if (val === undefined) { return store.remove(key); }
+			storage.setItem(key, store.serialize(val));
+		};
+		store.get = function(key) { return store.deserialize(storage.getItem(key)); };
+		store.remove = function(key) { storage.removeItem(key); };
+		store.clear = function() { storage.clear(); };
 		store.getAll = function() {
-			var ret = {}
+			var ret = {};
 			for (var i=0; i<storage.length; ++i) {
-				var key = storage.key(i)
-				ret[key] = store.get(key)
+				var key = storage.key(i);
+				ret[key] = store.get(key);
 			}
-			return ret
-		}
+			return ret;
+		};
 	} else if (isGlobalStorageNameSupported()) {
-		storage = win[globalStorageName][win.location.hostname]
+		storage = win[globalStorageName][win.location.hostname];
 		store.set = function(key, val) {
-			if (val === undefined) { return store.remove(key) }
-			storage[key] = store.serialize(val)
-		}
-		store.get = function(key) { return store.deserialize(storage[key] && storage[key].value) }
-		store.remove = function(key) { delete storage[key] }
-		store.clear = function() { for (var key in storage ) { delete storage[key] } }
+			if (val === undefined) { return store.remove(key); }
+			storage[key] = store.serialize(val);
+		};
+		store.get = function(key) { return store.deserialize(storage[key] && storage[key].value); };
+		store.remove = function(key) { delete storage[key]; };
+		store.clear = function() { for (var key in storage ) { delete storage[key]; } };
 		store.getAll = function() {
-			var ret = {}
+			var ret = {};
 			for (var i=0; i<storage.length; ++i) {
-				var key = storage.key(i)
-				ret[key] = store.get(key)
+				var key = storage.key(i);
+				ret[key] = store.get(key);
 			}
-			return ret
-		}
+			return ret;
+		};
 
 	} else if (doc.documentElement.addBehavior) {
 		var storageOwner,
-			storageContainer
+			storageContainer;
 		// Since #userData storage applies only to specific paths, we need to
 		// somehow link our data to a specific path.  We choose /favicon.ico
 		// as a pretty safe option, since all browsers already make a request to
@@ -115,80 +115,82 @@
 		// document can be used instead of the current document (which would
 		// have been limited to the current path) to perform #userData storage.
 		try {
-			storageContainer = new ActiveXObject('htmlfile')
-			storageContainer.open()
-			storageContainer.write('<s' + 'cript>document.w=window</s' + 'cript><iframe src="/favicon.ico"></frame>')
-			storageContainer.close()
-			storageOwner = storageContainer.w.frames[0].document
-			storage = storageOwner.createElement('div')
+			storageContainer = new ActiveXObject('htmlfile');
+			storageContainer.open();
+			storageContainer.write('<s' + 'cript>document.w=window</s' + 'cript><iframe src="/favicon.ico"></frame>');
+			storageContainer.close();
+			storageOwner = storageContainer.w.frames[0].document;
+			storage = storageOwner.createElement('div');
 		} catch(e) {
 			// somehow ActiveXObject instantiation failed (perhaps some special
 			// security settings or otherwse), fall back to per-path storage
-			storage = doc.createElement('div')
-			storageOwner = doc.body
+			storage = doc.createElement('div');
+			storageOwner = doc.body;
 		}
-		function withIEStorage(storeFunction) {
+		var withIEStorage = function(storeFunction) {
 			return function() {
-				var args = Array.prototype.slice.call(arguments, 0)
-				args.unshift(storage)
+				var args = Array.prototype.slice.call(arguments, 0);
+				args.unshift(storage);
 				// See http://msdn.microsoft.com/en-us/library/ms531081(v=VS.85).aspx
 				// and http://msdn.microsoft.com/en-us/library/ms531424(v=VS.85).aspx
-				storageOwner.appendChild(storage)
-				storage.addBehavior('#default#userData')
-				storage.load(localStorageName)
-				var result = storeFunction.apply(store, args)
-				storageOwner.removeChild(storage)
-				return result
-			}
-		}
-		function ieKeyFix(key) {
+				storageOwner.appendChild(storage);
+				storage.addBehavior('#default#userData');
+				storage.load(localStorageName);
+				var result = storeFunction.apply(store, args);
+				storageOwner.removeChild(storage);
+				return result;
+			};
+		};
+		var ieKeyFix = function(key) {
 			// In IE7, keys may not begin with numbers.
 			// See https://github.com/marcuswestin/store.js/issues/40#issuecomment-4617842
-			return '_'+key
-		}
+			return '_'+key;
+		};
 		store.set = withIEStorage(function(storage, key, val) {
-			key = ieKeyFix(key)
-			if (val === undefined) { return store.remove(key) }
-			storage.setAttribute(key, store.serialize(val))
-			storage.save(localStorageName)
-		})
+			key = ieKeyFix(key);
+			if (val === undefined) { return store.remove(key); }
+			storage.setAttribute(key, store.serialize(val));
+			storage.save(localStorageName);
+		});
 		store.get = withIEStorage(function(storage, key) {
-			key = ieKeyFix(key)
-			return store.deserialize(storage.getAttribute(key))
-		})
+			key = ieKeyFix(key);
+			return store.deserialize(storage.getAttribute(key));
+		});
 		store.remove = withIEStorage(function(storage, key) {
-			key = ieKeyFix(key)
-			storage.removeAttribute(key)
-			storage.save(localStorageName)
-		})
+			key = ieKeyFix(key);
+			storage.removeAttribute(key);
+			storage.save(localStorageName);
+		});
 		store.clear = withIEStorage(function(storage) {
-			var attributes = storage.XMLDocument.documentElement.attributes
-			storage.load(localStorageName)
-			for (var i=0, attr; attr=attributes[i]; i++) {
-				storage.removeAttribute(attr.name)
+			var attributes = storage.XMLDocument.documentElement.attributes;
+			storage.load(localStorageName);
+            var i = 0;
+			for (i, attr; (attr = attributes[i] !== null); i++) {
+				storage.removeAttribute(attr.name);
 			}
-			storage.save(localStorageName)
-		})
+			storage.save(localStorageName);
+		});
 		store.getAll = withIEStorage(function(storage) {
-			var attributes = storage.XMLDocument.documentElement.attributes
-			storage.load(localStorageName)
-			var ret = {}
-			for (var i=0, attr; attr=attributes[i]; ++i) {
-				ret[attr] = store.get(attr)
+			var attributes = storage.XMLDocument.documentElement.attributes;
+			storage.load(localStorageName);
+			var ret = {},
+                i = 0;
+			for (i, attr; (attr = attributes[i] !== null); ++i) {
+				ret[attr] = store.get(attr);
 			}
-			return ret
-		})
+			return ret;
+		});
 	}
 
 	try {
-		store.set(namespace, namespace)
-		if (store.get(namespace) != namespace) { store.disabled = true }
-		store.remove(namespace)
+		store.set(namespace, namespace);
+		if (store.get(namespace) != namespace) { store.disabled = true; }
+		store.remove(namespace);
 	} catch(e) {
-		store.disabled = true
+		store.disabled = true;
 	}
 	
-	if (typeof module != 'undefined') { module.exports = store }
-	else if (typeof define === 'function' && define.amd) { define(store) }
-	else { this.store = store }
-})()
+	if (typeof module != 'undefined') { module.exports = store; }
+	else if (typeof define === 'function' && define.amd) { define(store); }
+	else { this.store = store; }
+})();
